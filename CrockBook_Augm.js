d521b95653229e8288035196248988cfecf8cdc0
Yel <yel@film.ch>
2017-05-10 19:54:08 +0200

    TEST

--- CrockBook_Augm.js
+++ CrockBook_Augm.js
@@ -24,12 +24,13 @@ console.log(Number(-2.9).integer());  // -2
 console.log(Number(2.9).integer());   //  2
 console.log(typeof (-10 / 3)  === 'number'); // true
 console.log((-10 / 3).integer()); // 3
+console.log('Stefan ist da');
 
 
 // Weiteres Beispiel:
 
 Object.method('makeInt', function() {
-    
+
     //return typeof this.a === 'number' ? this.a : parseInt(this.a);  // return A
     return typeof this === 'number' ? this : parseInt(this);  // return B
 });
@@ -39,5 +40,3 @@ console.log({a:42}.makeInt()); // 42 --> läuft mit return A
 
 console.log(Object("4.3").makeInt() * 3);  // 12  --> --> läuft mit return B
 console.log(Object(45).makeInt());  // 45  --> läuft mit return B
-
-
